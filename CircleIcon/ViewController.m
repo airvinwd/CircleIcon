9bc6967b5f46c8f6b9a28dc364d6565dce69ed51
吴迪 <wudi@superid.cn>
2017-03-11 17:12:05 +0800

    Initial Commit

---
+++ CircleIcon/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CircleIcon
+//
+//  Created by 吴迪 on 2017/3/11.
+//  Copyright © 2017年 吴迪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
